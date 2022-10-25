99bc7db8d5c79c1c2117836c105f75be284f5d51
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2022-10-25 15:14:05 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Users CRUD
+This is an app I created for Coding Dojo. I learned how to create an app which uses CRUD using Flask and MySQL.
+### Objectives:
+* Use the same Flask project from the Users CR assignment
+* On the Read (All) page add an Actions column
+* Show link will render the Users Read (One) page
+* Edit link will render the Users Edit page
+* Delete link will delete the User from the database, and redirect to the Read (All) page
+* After successful creation of a new User, redirect to Read (One) page
+* Read (One) page will display the User's information
+* Read (One) page to have Edit and Delete links
+* Edit page will have a form pre-populated with the users information
+* Update the updated_at field when editing the User's information
+* After successful update of user, redirect to the Read (One) page and display the updated information
+* All Home links should redirect to the Read (All) page
+
+## I was given this wireframe as a reference:
+<img src='flask_app/static/img/ref.png' height='1000'>
---
+++ flask_app/models/user.py
@@ -0,0 +1,43 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+class User:
+    def __init__(self, data) -> None:
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        
+    @classmethod
+    def show_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('users_schema').query_db(query)
+        users = []
+        
+        for user in results:
+            users.append( cls(user) )
+        return users
+    
+    @classmethod
+    def show_one(cls, data):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return result
+    
+    @classmethod
+    def edit_one(cls, data):
+        query = "UPDATE users SET first_name = %(fname)s, last_name = %(lname)s, email = %(email)s WHERE id = %(id)s"
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return result
+    
+    @classmethod
+    def delete_one(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s"
+        result = connectToMySQL('users_schema').query_db(query, data)
+        return result
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email) VALUES (%(fname)s, %(lname)s, %(email)s);"
+        return connectToMySQL('users_schema').query_db(query, data)
